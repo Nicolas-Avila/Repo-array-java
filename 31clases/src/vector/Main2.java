348de77fa284a8289cfd5f157d7722a44466e129
avila nicolas <aviladanielnicolas@gmail.com>
2022-03-31 20:52:38 -0300

    muestra mayor

--- 31clases/src/vector/Main2.java
+++ 31clases/src/vector/Main2.java
@@ -16,19 +16,30 @@ public class Main2 {
         promedio de todos sus numeors */
         
         int num[] = new int[10];
-        int total=0;
+        int total=0, mayor=0;
        
         Scanner entrada=new Scanner(System.in);
                 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println("Ingrese el numero en la posicion: "+(i+1));
             num[i]=entrada.nextInt();
             total=num[1]+num[0];
+        
+            if (num[i]>mayor) {
+            mayor=num[i];
         }
+            
+        }
+        
+        
+        
+        
+        
         System.out.println("el total es: "+total);
+        System.out.println("el mayor es: "+mayor);
         
         
         
-        
+         
 }
 }
