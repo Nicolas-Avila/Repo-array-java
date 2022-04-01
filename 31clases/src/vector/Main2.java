cc11367d8733f5f18e4e94fd610c4406bdf1c842
avila nicolas <aviladanielnicolas@gmail.com>
2022-03-31 21:30:24 -0300

    bien hecho muestra mayor, menor, promedio, sumatotal

--- 31clases/src/vector/Main2.java
+++ 31clases/src/vector/Main2.java
@@ -16,7 +16,7 @@ public class Main2 {
         promedio de todos sus numeors */
         
         int num[] = new int[10];
-        int total=0, mayor=0,menor=10000;
+        int total=0, mayor=0,menor=10000,promedio=0, cant=0;
        
         Scanner entrada=new Scanner(System.in);
                 
@@ -25,27 +25,29 @@ public class Main2 {
             System.out.println("Ingrese el numero en la posicion: "+(i+1));
             num[i]=entrada.nextInt();
             total+=num[i];
-                  
             
-        }
-        
+              cant++;    
+            }
+        promedio = total / cant;
         mayor=num[0];
         menor=num[0];
         for (int i = 0; i < 3; i++) {
-            
-        
+                   
          if (num[i]>mayor) {
             mayor=num[i];
             }
             if(menor>num[i]){
                 menor=num[i];
             }
+            
+            
         }
         
         
         System.out.println("el total es: "+total);
         System.out.println("el mayor es: "+mayor);
         System.out.println("el menor es: "+menor);
+        System.out.println("el promedio es "+promedio);
         
         
         
