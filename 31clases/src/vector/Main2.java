172ada0e6f0deed62df76c94102ba574ada3453d
avila nicolas <aviladanielnicolas@gmail.com>
2022-03-31 21:17:44 -0300

    suma total mayor y menor bien hecho

--- 31clases/src/vector/Main2.java
+++ 31clases/src/vector/Main2.java
@@ -16,27 +16,36 @@ public class Main2 {
         promedio de todos sus numeors */
         
         int num[] = new int[10];
-        int total=0, mayor=0;
+        int total=0, mayor=0,menor=10000;
        
         Scanner entrada=new Scanner(System.in);
                 
         for (int i = 0; i < 3; i++) {
+            
             System.out.println("Ingrese el numero en la posicion: "+(i+1));
             num[i]=entrada.nextInt();
-            total=num[1]+num[0];
-        
-            if (num[i]>mayor) {
-            mayor=num[i];
-        }
+            total+=num[i];
+                  
             
         }
         
+        mayor=num[0];
+        menor=num[0];
+        for (int i = 0; i < 3; i++) {
+            
         
-        
+         if (num[i]>mayor) {
+            mayor=num[i];
+            }
+            if(menor>num[i]){
+                menor=num[i];
+            }
+        }
         
         
         System.out.println("el total es: "+total);
         System.out.println("el mayor es: "+mayor);
+        System.out.println("el menor es: "+menor);
         
         
         
