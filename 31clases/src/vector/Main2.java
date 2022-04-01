9745893c03382f2dcd8f2cf27fae0e77113720b4
avila nicolas <aviladanielnicolas@gmail.com>
2022-03-31 22:03:52 -0300

    muestra el resto y el orden el array

--- 31clases/src/vector/Main2.java
+++ 31clases/src/vector/Main2.java
@@ -14,24 +14,23 @@ public class Main2 {
     public static void main(String[] args) {
         /*vector de 10 numeros cargarlo, mostrarlo numero mayor numero menor la suma de todos }
         promedio de todos sus numeors */
-        
+               
         int num[] = new int[10];
         int total=0, mayor=0,menor=10000,promedio=0, cant=0;
        
         Scanner entrada=new Scanner(System.in);
                 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
             
             System.out.println("Ingrese el numero en la posicion: "+(i+1));
             num[i]=entrada.nextInt();
             total+=num[i];
-            
-              cant++;    
-            }
+            cant++;    
+        }
         promedio = total / cant;
         mayor=num[0];
         menor=num[0];
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
                    
          if (num[i]>mayor) {
             mayor=num[i];
@@ -39,18 +38,37 @@ public class Main2 {
             if(menor>num[i]){
                 menor=num[i];
             }
-            
-            
-        }
-        
-        
+                        
+        } 
         System.out.println("el total es: "+total);
         System.out.println("el mayor es: "+mayor);
         System.out.println("el menor es: "+menor);
         System.out.println("el promedio es "+promedio);
         
+         /*ordenar de menor a mayor*/
+         int aux=0;
+         
+         for (int i = 0; i < 10; i++) {
+             for (int j = 0; j < 10; j++) {
+                 if (num[i]<num[j]) {
+                     aux=num[i];
+                     num[i]=num[j];
+                     num[j]=aux;
+                     
+                 }
+         
+             }
+          }
+         System.out.println("el orden es:");
+         for (int i = 0; i < 10; i++) {
+             
+            System.out.println(""+num[i]);
+        }
         
-        
+    
+            
+             
+         
          
 }
 }
